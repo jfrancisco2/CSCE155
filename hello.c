f6cfdc44238487e1c25bfcd3280d55d5af84727c
jfrancisco2 <42565502+jfrancisco2@users.noreply.github.com>
2018-08-23 11:51:42 -0500

    Add files via upload

---
+++ hello.c
@@ -0,0 +1,16 @@
+/**
+ * Author: Juan L. Francisco & Amanda Whitlatch
+ * Date: 2018/08/21
+ *
+ * A simple hello world program in C
+ *
+ */
+#include<stdlib.h>
+#include<stdio.h>
+
+int main(int argc, char **argv) {
+
+  printf("Juan L Francisco & Computer Science\n");
+
+  return 0;
+}
